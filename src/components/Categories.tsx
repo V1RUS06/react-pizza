2090947f95bdaae3c4b9be71837fd146f138a710
Denis <denison4@mail.ru>
2022-05-21 20:08:51 +0300

    Рендер категорий

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Categories = () => {
+  const [activeIndex, setActiveIndex] = useState<number>(0);
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+
+  const onCategoriesClick = (index: number) => {
+    setActiveIndex(index);
+  };
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((value, index) => (
+          <li
+            onClick={() => onCategoriesClick(index)}
+            className={activeIndex === index ? "active" : ""}
+            key={index}
+          >
+            {value}
+          </li>
+        ))}
       </ul>
     </div>
   );
